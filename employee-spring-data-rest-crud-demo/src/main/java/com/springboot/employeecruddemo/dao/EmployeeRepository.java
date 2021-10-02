ce39376aea6c5f845cbc58d6f23daeb658575a44
Tony Chao <tchao@mail.usf.edu>
2021-10-02 18:49:28 -0400

    Added Custom HTTP Endpoint for finding Employee by lastName

--- employee-spring-data-rest-crud-demo/src/main/java/com/springboot/employeecruddemo/dao/EmployeeRepository.java
+++ employee-spring-data-rest-crud-demo/src/main/java/com/springboot/employeecruddemo/dao/EmployeeRepository.java
@@ -3,14 +3,17 @@ package com.springboot.employeecruddemo.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.springboot.employeecruddemo.entity.Employee;
 
 // http://localhost:8080/api/members instead of http://localhost:8080/api/employees
 // @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	@Query("SELECT e FROM Employee e WHERE e.lastName LIKE ?1%")
-	List<Employee> findEmployeeByLastName(String lastName);
+	// Custom HTTP Endpoint: http://localhost:/api/employees/search/byLastName?lastName
+	// Without @RestResource: http://localhost:8080/api/employees/search/findByLastName?lastName
+	// @RestResource(path = "byLastName", rel = "customFindMethod")
+	// @Param uses the entity class (Employee) properties and calls appropriate getter/setter methods
+	List<Employee> findByLastName(@Param("lastName") String lastName);
 }
