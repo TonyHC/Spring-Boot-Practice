84f4b34e70f2e32ff68427e0ca65da571663f9e3
Tony Chao <tchao@mail.usf.edu>
2021-11-08 22:31:17 -0500

    Setup the jms embedded server, task executor and message converter configurations

--- jms/src/main/java/com/springframework/jms/JmsApplication.java
+++ jms/src/main/java/com/springframework/jms/JmsApplication.java
@@ -1,13 +1,21 @@
 package com.springframework.jms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsApplication {
+    public static void main(String[] args) throws Exception {
+        // Embedded ActiveMQ Server (normally run outside of Spring Boot, could use Docker to run ActiveMQ Server)
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));
 
-    public static void main(String[] args) {
         SpringApplication.run(JmsApplication.class, args);
     }
-
 }
